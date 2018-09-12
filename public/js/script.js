3c5068110ac9364b8fb2b49e8ac33ee9c4730ee6
Jessie Beech <jessie@jessiebeech.com>
2018-09-12 16:58:34 +0100

    add basic css and front end fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,27 @@
 /* eslint-disable */
 var chatUl = document.getElementById('chat-ul');
-var chatLi = document.createElement('li');
 var sendButton = document.getElementById('send-button');
-var userInput = document.getElementById('user-input')
 var thinkingDiv = document.getElementById('thinking-bubble');
 
 sendButton.addEventListener('click', function(e) {
+  thinkingDiv.textContent = "";
+  var userInput = document.getElementById('user-input')
   e.preventDefault();
+  // add message bubble
+  var chatLi = document.createElement('li');
+  chatLi.id = "user-speech-bubble";
   chatLi.innerText = userInput.value;
+  // include subtext of bot/user
+  var ptext = document.createElement('p');
+  ptext.textContent = "User";
+  ptext.classList.add("name");
+  chatLi.appendChild(ptext);
   chatUl.appendChild(chatLi);
-
-  for ( var i = 0; i < 3 ; i++ ) {
-    var li = document.createElement('li');
-    li.innerText = ('.');
-    thinkingDiv.appendChild(li);
-  }
-
+  // add thinking div
+  thinkingDiv.textContent = "thinking..."
+  // clear input, reset focus
+  userInput.value = ""
+  userInput.focus();
 });
 
+//<p class="name">User</p>
