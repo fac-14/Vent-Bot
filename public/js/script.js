bf0b11843ae7dbf322ccacb8a0b61c7b82d1cb66
Jessie Beech <jessie@jessiebeech.com>
2018-09-12 16:40:00 +0100

    test solution

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,20 @@
 /* eslint-disable */
-
+var chatUl = document.getElementById('chat-ul');
+var chatLi = document.createElement('li');
 var sendButton = document.getElementById('send-button');
-console.log(sendButton);
+var userInput = document.getElementById('user-input')
+var thinkingDiv = document.getElementById('thinking-bubble');
+
 sendButton.addEventListener('click', function(e) {
   e.preventDefault();
-  // console.log('you clicked the button');
-  var userInput = document.getElementById('user-input');
-  userInput.value;
-  // console.log(userInput.value);
-
-  var chatUl = document.getElementById('chat-ul');
-  var chatLi = document.createElement('li');
   chatLi.innerText = userInput.value;
-  // console.log(chatLi.innerText);
   chatUl.appendChild(chatLi);
 
-  var thinkingDiv = document.getElementById('thinking-bubble');
-  var thinkingLi1 = document.createElement('li');
-  var thinkingLi2 = document.createElement('li');
-  var thinkingLi3 = document.createElement('li');
-  
-  thinkingLi1.innerText = ('.');
-  thinkingLi2.innerText = ('.');
-  thinkingLi3.innerText = ('.');
+  for ( var i = 0; i < 3 ; i++ ) {
+    var li = document.createElement('li');
+    li.innerText = ('.');
+    thinkingDiv.appendChild(li);
+  }
 
-  thinkingDiv.appendChild(thinkingLi1);
-  thinkingDiv.appendChild(thinkingLi2);
-  thinkingDiv.appendChild(thinkingLi3);
-
-
-  
 });
 
