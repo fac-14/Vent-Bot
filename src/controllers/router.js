4ce6cb4878fa5f3e6267c2ce72e76f9d38479b95
harryyy27 <harryyy27@gmail.com>
2018-09-12 14:36:02 +0100

    404 error working

--- src/controllers/router.js
+++ src/controllers/router.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+//import handlers
+
 const home = require('./routes/home');
 const chat = require('./routes/chat');
 const about = require('./routes/about');
 const resources = require('./routes/resources');
 const message = require('./routes/message');
+const error = require('./routes/error');
+
+//routes
 
 router.get("/", home.get);
 router.get("/chat", chat.get);
 router.get("/about", about.get);
 router.get("/resources", resources.get);
 router.post("/send-message", message.post);
+
+//error
+
+router.use(error.get404);
+router.use(error.get500);
 
 module.exports = router;
