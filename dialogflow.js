bfd8a519176538946b9f20d7296866dc5c2e609b
Jessie Beech <jessie@jessiebeech.com>
2018-09-13 12:24:22 +0100

    link up to dialogflow

---
+++ dialogflow.js
@@ -0,0 +1,44 @@
+const env = require('env2')('./config.env');
+
+// You can find your project ID in your Dialogflow agent settings
+const projectId = process.env.PROJECT_ID; 
+const sessionId = process.env.SESSION_ID;
+
+const query = "hello";
+const languageCode = "en-US";
+
+// Instantiate a DialogFlow client.
+const dialogflow = require("dialogflow");
+const sessionClient = new dialogflow.SessionsClient();
+
+// Define session path
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+// The text query request.
+const request = {
+  session: sessionPath,
+  queryInput: {
+    text: {
+      text: query,
+      languageCode
+    }
+  }
+};
+
+// Send request and log result
+sessionClient
+  .detectIntent(request)
+  .then(responses => {
+    console.log("Detected intent");
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+      console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+      console.log(`  No intent matched.`);
+    }
+  })
+  .catch(err => {
+    console.error("ERROR:", err);
+  });
--- src/controllers/routes/message.js
+++ src/controllers/routes/message.js
@@ -1,8 +1,8 @@
 exports.post = (req, res) => {
-    const message = req.body.message;
-    console.log(message);
-    // send message to Dialog Flow API
-
-    res.redirect(302, '/chat');
-    res.end();
-}
+  const message = req.body.message;
+  console.log(message);
+  // send message to Dialog Flow API
+  
+  res.redirect(302, "/chat");
+  res.end();
+};
