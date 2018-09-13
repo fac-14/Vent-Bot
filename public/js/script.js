107edc727f588dc6d5c02b9c0546de7f75015325
Jessie Beech <jessie@jessiebeech.com>
2018-09-13 11:20:32 +0100

    post request coming through

--- public/js/script.js
+++ public/js/script.js
@@ -4,13 +4,19 @@ var sendButton = document.getElementById('send-button');
 var thinkingDiv = document.getElementById('thinking-bubble');
 
 sendButton.addEventListener('click', function(e) {
-  thinkingDiv.textContent = "";
-  var userInput = document.getElementById('user-input')
   e.preventDefault();
+  // clear previous "thinking..."
+  thinkingDiv.textContent = "";
+  // get input
+  var inputField = document.getElementById('user-input')
+  var userInput = inputField.value;
+  console.log(userInput);
+  // Send input to server
+  sendInput(userInput);
   // add message bubble
   var chatLi = document.createElement('li');
   chatLi.id = "user-speech-bubble";
-  chatLi.innerText = userInput.value;
+  chatLi.innerText = userInput;
   // include subtext of bot/user
   var ptext = document.createElement('p');
   ptext.textContent = "User";
@@ -21,7 +27,20 @@ sendButton.addEventListener('click', function(e) {
   thinkingDiv.textContent = "thinking..."
   // clear input, reset focus
   userInput.value = ""
-  userInput.focus();
+  inputField.focus();
+  // e.dispatchEvent();
 });
 
-//<p class="name">User</p>
+function sendInput(input) {
+  var encoded = "message=" + encodeURI(input);
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.statusCode === 200) {
+      var parsed = JSON.parse(xhr.responseText)
+      console.log(parsed);
+    }
+  }
+  xhr.open("POST","/send-message", true)
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(encoded)
+}
--- src/controllers/routes/message.js
+++ src/controllers/routes/message.js
@@ -1,5 +1,5 @@
 exports.post = (req, res) => {
     console.log(req.body);
-    res.redirect(302, '/');
+    res.redirect(302, '/chat');
     res.end();
 }
