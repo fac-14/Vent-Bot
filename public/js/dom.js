6dcc05437995b89dc20cdff4f1d698754a2dde24
Jessie Beech <jessie@jessiebeech.com>
2018-09-20 16:17:39 +0100

    made sexy menu animation

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,34 +1,37 @@
 //navbar visibility
 
-const menu = document.getElementById('pop-out-menu');
+const menu = document.querySelector('#pop-out-menu');
 const navIcon = document.querySelector('.nav-icon');
+const menuLines = document.querySelectorAll('.menu-line');
+const exit = document.getElementById("exit-btn");
+const overlay = document.getElementsByClassName("overlay");
+const no = document.getElementById("modal-no");
+const yes = document.getElementById("modal-yes");
 
 navIcon.addEventListener('click', showMenu)
 function showMenu() {
   if (menu.className === 'show-menu') {
     menu.classList.remove('show-menu');
+    menuLines.forEach((line) => {
+      line.classList.remove("arrow-formation");
+    })
   } else {
     menu.classList.add('show-menu');
+    menuLines.forEach((line) => {
+      line.classList.add("arrow-formation");
+    })
   }
 }
 
-// for modal functionality
 // open modal
-const exit = document.getElementById("exit-btn");
-const overlay = document.getElementsByClassName("overlay");
 exit.addEventListener("click", (e) => {
   overlay[0].classList.add("show");
 });
-
 // close modal - answer No to question
-const no = document.getElementById("modal-no");
 no.addEventListener("click", (e) => {
   overlay[0].classList.remove("show");
 });
-
 // reroute to set if free screen - answe Yes to question
-const yes = document.getElementById("modal-yes");
 yes.addEventListener("click", (e) => {
   window.location = "/animation";
 });
-
